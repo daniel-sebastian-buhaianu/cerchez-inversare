701e29f51d4e59ab32d5b3f13d47a308e302ab2b
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-10-23 14:57:18 +0300

    Update inversare.cpp
    cerinta 2

--- inversare.cpp
+++ inversare.cpp
@@ -1,14 +1,41 @@
 #include <fstream>
+#include <cstring>
+#define LGMAX 20
 using namespace std;
 ifstream fin("inversare.in");
 ofstream fout("inversare.out");
 void inversare();
+void inversare2();
 int main()
 {
-	inversare();
+	//inversare();
+	inversare2();
 	fin.close();
 	fout.close();
 	return 0;
+}
+bool esteLitera(char c)
+{
+	return (c >= 'a' && c <= 'z')
+		|| (c >= 'A' && c <= 'Z');
+}
+void inversare2()
+{
+	char cuv[LGMAX];
+	int lg;
+	fin.getline(cuv, LGMAX, ' ');
+	lg = strlen(cuv);
+	if (esteLitera(cuv[lg-1]))
+	{
+		fout << cuv << ' ';
+		inversare2();
+	}
+	else
+	{
+		cuv[lg-2] = 0;
+		fout << cuv << ' ';
+	}
+	fout << cuv << ' ';
 }
 void inversare()
 {
